4b816bb2f0992fa8c9f5616c4675ac5705b0ec77
ncouret <nathanael.couret@gmail.com>
2016-05-24 23:05:09 +0200

    customizing authentication

---
+++ eBeer/app/controllers/Register.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created by Ataw on 22/05/2016.
+ */
+public class Register extends Controller {
+
+    public static void signup() {
+        render();
+    }
+
+    public static void register() {
+
+    }
+}
---
+++ eBeer/app/controllers/security/AuthSecurity.java
@@ -0,0 +1,19 @@
+package controllers.security;
+
+import controllers.Secure;
+
+/**
+ * Created by Ataw on 21/05/2016.
+ */
+public class AuthSecurity extends Secure.Security {
+
+    /**
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    static boolean authenticate(String username, String password) {
+        return false;
+    }
+}
