4e9fbbd153ac3aeac17c1fa0aa9c5b9482eced7e
Jean de la Fayolle <jean.de-la-fayolle@ensimag.grenoble-inp.fr>
2016-05-25 16:58:01 +0200

    Tests API

--- eBeer/app/controllers/Application.java
+++ eBeer/app/controllers/Application.java
@@ -1,17 +1,43 @@
 package controllers;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import controllers.beer.BrewAPIAccess;
 import play.mvc.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		render();
+	}
 
-    public static void status() {
-        boolean status = BrewAPIAccess.isAlive();
-        render(status);
-    }
+	public static void status() {
+		boolean status = BrewAPIAccess.isAlive();
+		render(status);
+	}
+
+	public static void testBeers() {
+		JsonElement beersJson = BrewAPIAccess.getAllBeers();
+		JsonObject jsonObject = beersJson.getAsJsonObject();
+		JsonElement jsonElement = jsonObject.get("data");
+		JsonArray jsonArray = jsonElement.getAsJsonArray();
+		for (int i = 0; i < jsonArray.size(); i++) {
+			jsonArray.get(i).getAsJsonObject().remove("style");
+			jsonArray.get(i).getAsJsonObject().remove("nameDisplay");
+			jsonArray.get(i).getAsJsonObject().remove("srm");
+			jsonArray.get(i).getAsJsonObject().remove("glass");
+			jsonArray.get(i).getAsJsonObject().remove("glasswareId");
+			jsonArray.get(i).getAsJsonObject().remove("srmId");
+			jsonArray.get(i).getAsJsonObject().remove("styleId");
+			jsonArray.get(i).getAsJsonObject().remove("isOrganic");
+			jsonArray.get(i).getAsJsonObject().remove("status");
+			jsonArray.get(i).getAsJsonObject().remove("statusDisplay");
+			jsonArray.get(i).getAsJsonObject().remove("createDate");
+			jsonArray.get(i).getAsJsonObject().remove("updateDate");
+		}
+		renderJSON(jsonArray);
+	}
 
 }
--- eBeer/app/controllers/beer/BrewAPIAccess.java
+++ eBeer/app/controllers/beer/BrewAPIAccess.java
@@ -1,5 +1,7 @@
 package controllers.beer;
 
+import com.google.gson.JsonElement;
+
 import play.libs.WS;
 
 /**
@@ -19,6 +21,30 @@ public final class BrewAPIAccess {
             return true;
         }
         return false;
+    }
+    
+    public static JsonElement getAllBeers() {
+    	WS.WSRequest request = WS.url(SERVICE_URL + "beers");
+    	request.parameters.put("key", API_KEY);
+    	request.parameters.put("abv", "0,20");
+    	request.parameters.put("p", 1);
+    	request.parameters.put("format", "json");
+    	WS.HttpResponse res = request.get();
+    	if(res.success()) {
+    		return res.getJson();
+    	}
+    	return null;
+    }
+    
+    public static JsonElement getBeerById(String id) {
+    	WS.WSRequest request = WS.url(SERVICE_URL + "beer/" + id);
+    	request.parameters.put("key", API_KEY);
+    	request.parameters.put("format", "json");
+    	WS.HttpResponse res = request.get();
+    	if(res.success()) {
+    		return res.getJson();
+    	}
+    	return null;
     }
 
 }
