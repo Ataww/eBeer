304b531e221855e79f0e0fc9aba8bae7c43fa910
Nathanael Couret <Nathanael.Couret@ensimag.grenoble-inp.fr>
2016-06-01 09:13:33 +0200

    internationalization on the way

--- eBeer/app/controllers/Application.java
+++ eBeer/app/controllers/Application.java
@@ -9,39 +9,37 @@ import play.mvc.*;
 
 public class Application extends Controller {
 
-	public static void status() {
-		boolean status = BrewAPIAccess.isAlive();
-		render(status);
-	}
-    public static void index() {
-        if (session.contains("username")) {
-            String username = session.get("username");
-            render(username);
-        } else {
-            render();
-        }
+    public static void status() {
+        boolean status = BrewAPIAccess.isAlive();
+        String username = session.get("username");
+        render(status,username);
     }
 
-	public static void testBeers() {
-		JsonElement beersJson = BrewAPIAccess.getAllBeers();
-		JsonObject jsonObject = beersJson.getAsJsonObject();
-		JsonElement jsonElement = jsonObject.get("data");
-		JsonArray jsonArray = jsonElement.getAsJsonArray();
-		for (int i = 0; i < jsonArray.size(); i++) {
-			jsonArray.get(i).getAsJsonObject().remove("style");
-			jsonArray.get(i).getAsJsonObject().remove("nameDisplay");
-			jsonArray.get(i).getAsJsonObject().remove("srm");
-			jsonArray.get(i).getAsJsonObject().remove("glass");
-			jsonArray.get(i).getAsJsonObject().remove("glasswareId");
-			jsonArray.get(i).getAsJsonObject().remove("srmId");
-			jsonArray.get(i).getAsJsonObject().remove("styleId");
-			jsonArray.get(i).getAsJsonObject().remove("isOrganic");
-			jsonArray.get(i).getAsJsonObject().remove("status");
-			jsonArray.get(i).getAsJsonObject().remove("statusDisplay");
-			jsonArray.get(i).getAsJsonObject().remove("createDate");
-			jsonArray.get(i).getAsJsonObject().remove("updateDate");
-		}
-		renderJSON(jsonArray);
-	}
+    public static void index() {
+        String username = session.get("username");
+        render(username);
+    }
+
+    public static void testBeers() {
+        JsonElement beersJson = BrewAPIAccess.getAllBeers();
+        JsonObject jsonObject = beersJson.getAsJsonObject();
+        JsonElement jsonElement = jsonObject.get("data");
+        JsonArray jsonArray = jsonElement.getAsJsonArray();
+        for (int i = 0; i < jsonArray.size(); i++) {
+            jsonArray.get(i).getAsJsonObject().remove("style");
+            jsonArray.get(i).getAsJsonObject().remove("nameDisplay");
+            jsonArray.get(i).getAsJsonObject().remove("srm");
+            jsonArray.get(i).getAsJsonObject().remove("glass");
+            jsonArray.get(i).getAsJsonObject().remove("glasswareId");
+            jsonArray.get(i).getAsJsonObject().remove("srmId");
+            jsonArray.get(i).getAsJsonObject().remove("styleId");
+            jsonArray.get(i).getAsJsonObject().remove("isOrganic");
+            jsonArray.get(i).getAsJsonObject().remove("status");
+            jsonArray.get(i).getAsJsonObject().remove("statusDisplay");
+            jsonArray.get(i).getAsJsonObject().remove("createDate");
+            jsonArray.get(i).getAsJsonObject().remove("updateDate");
+        }
+        renderJSON(jsonArray);
+    }
 
 }
